59b160a0208d1c9102a8303f8d540a3868c1a47b
VasilchenkoSergei <chainsaw86@mail.ru>
2018-08-31 00:38:38 +0300

    up button done

--- scripts/script.js
+++ scripts/script.js
@@ -14,4 +14,22 @@ $(document).ready(function(){
   $('.main-nav').toggle();
     });
 
+  $(function() {
+    $(window).scroll(function() {
+       if($(this).scrollTop() != 0) {
+          $('#toTop').fadeIn();
+        } else {
+          $('#toTop').fadeOut();
+        }
+                });
+
+  $('#toTop').click(function() {
+    $('body,html').animate({scrollTop:0},800);
+    });
+  });
+
+
+
+
 })();
+
