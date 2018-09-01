95cc234ee4a3c3036671f2d6547deb9d59dd3918
VasilchenkoSergei <chainsaw86@mail.ru>
2018-09-01 23:13:36 +0300

    no comments included

--- scripts/script.js
+++ scripts/script.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
           $('#toTop').fadeIn();
         } else {
           $('#toTop').fadeOut();
-  }
+        }
   });
 
   $('#toTop').click(function() {
@@ -37,51 +37,16 @@ $(document).ready(function(){
 
   $('.appartments-item__star').on('click', function() {
   $(this).css({'background-image':'url(img/star-dark.png)'});
-  // $(this).css({'background-image':'url(img/star.png)'});
-    });
+  });
 
 
- // $('.appartments-item__star').click(function() { 
- //      $(this).toggle(function() { 
- //      $(this).css({'background-image':'url(img/star-dark.png)'}); 
-
- //      }); 
- //     }); 
-
-// $('.appartments-item__star').click(function() {
-//     $('.appartments-item__star').toggle();
-//     $(this).toggleClass('active');
-//     return false;
-// });
-
-// $('.appartments-item__star').click(function() {
-// $('.appartments-item__star').toggle(function () {
-//     $(".appartments-item__star").addClass(".appartments-item__star--active");
-// }, function () {
-//     $(".appartments-item__star").removeClass(".appartments-item__star--active");
-// });
-// });
 
 
   $('.appartments-item').on('click', function() {
   $(this).children('.appartments-footer').css({'background-color':'#a7a37e'});
   $(this).children('.appartments-footer').children('.appartments-footer__text--free').css({'display':'none'});
   $(this).children('.appartments-footer').children('.appartments-footer__text--reserved').css({'display':'block'});
-    });
-
-  // $('.appartments-item').on('click', function() {
-  // $(this).children('.appartments-footer').css({'background-color':'red'});
-  // $(this).children('.appartments-footer').children('.appartments-footer__text--free').css({'display':'block'});
-  // $(this).children('.appartments-footer').children('.appartments-footer__text--reserved').css({'display':'none'});
-  //   });
-
-  // $('.appartments-item').on('click', function() {
-  // $(this).children('.appartments-footer').toggleClass('.appartments-footer__active');
-  // $(this).children('.appartments-footer').children('.appartments-footer__text--free').toggleClass('.free--active');
-  // $(this).children('.appartments-footer').children('.appartments-footer__text--reserved').toggleClass('.reserved--active');
-  // $(this).toggle();
-  //   });
-
+  });
 
 
 })();
