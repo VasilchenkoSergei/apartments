e71d65640e7fa48afee8e3869b0d11a2888c88e2
VasilchenkoSergei <chainsaw86@mail.ru>
2018-09-01 15:51:23 +0300

    fixed icons

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 function validate(form_id,email) {
-   var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-   var address = document.forms[form_id].elements[email].value;
-   if(reg.test(address) == false) {
-      alert('Введите корректный e-mail');
-      return false;
+  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  var address = document.forms[form_id].elements[email].value;
+  if(reg.test(address) == false) {
+    alert('Введите корректный e-mail');
+    return false;
    }
 }
 
@@ -30,7 +30,7 @@ $(document).ready(function(){
 
   $('.main-nav__link').on('click', function() {
   $(this).css({'color': '#900060'});
-    });
+  });
 
 
 
