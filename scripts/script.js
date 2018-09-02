78cbe6433ac9f7bca3354356e1a9510d9695d0d6
VasilchenkoSergei <chainsaw86@mail.ru>
2018-09-02 16:24:03 +0300

    sprite added, js by click added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// ВАЛИДАЦИЯ EMAIL
+
 function validate(form_id,email) {
   var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
   var address = document.forms[form_id].elements[email].value;
@@ -7,6 +9,9 @@ function validate(form_id,email) {
    }
 }
 
+
+// БУРГЕР МЕНЮ
+
 $(document).ready(function(){
 
   $('.page-header__burger').on('click', function() {
@@ -14,39 +19,72 @@ $(document).ready(function(){
   $('.main-nav').toggle();
     });
 
+
+// КНОПКА ВВЕРХ
+
   $(function() {
+    $('#toTop').fadeOut();
     $(window).scroll(function() {
        if($(this).scrollTop() != 0) {
           $('#toTop').fadeIn();
         } else {
           $('#toTop').fadeOut();
         }
-  });
-
-  $('#toTop').click(function() {
-    $('body,html').animate({scrollTop:0},800);
     });
+
+    $('#toTop').click(function() {
+      $('body,html').animate({scrollTop:0},800);
+    });
+
   });
 
-  $('.main-nav__link').on('click', function() {
-  $(this).css({'color': '#900060'});
-  });
+
+
+// ПОСЕЩЕННЫЕ ССЫЛКИ
+
+  // $('.main-nav__link').on('click', function() {
+  // $(this).css({'color': '#900060'});
+  // });
 
 
 
+// ДОБАВЛЕНИЕ В ИЗБРАННОЕ
 
   $('.appartments-item__star').on('click', function() {
-  $(this).css({'background-image':'url(img/star-dark.png)'});
+
+    if ($(this).attr('click-state') == 1) {
+      $(this).attr('click-state', 0)
+      $(this).css({'background-position':'-10px -10px'})
+    } else {
+      $(this).attr('click-state', 1)
+      $(this).css({'background-position':'-50px -10px'})
+    }
+
   });
 
 
 
+
+// БРОНИРОВАНИЕ АППАРТАМЕНТОВ
 
   $('.appartments-item').on('click', function() {
-  $(this).children('.appartments-footer').css({'background-color':'#a7a37e'});
-  $(this).children('.appartments-footer').children('.appartments-footer__text--free').css({'display':'none'});
-  $(this).children('.appartments-footer').children('.appartments-footer__text--reserved').css({'display':'block'});
+
+    if ($(this).attr('click-state') == 1) {
+      $(this).attr('click-state', 0)
+      $(this).children('.appartments-footer').css({'background-color':'#002f2f'});
+      $(this).children('.appartments-footer').children('.appartments-footer__text--free').css({'display':'block'});
+      $(this).children('.appartments-footer').children('.appartments-footer__text--reserved').css({'display':'none'});
+    } else {
+      $(this).attr('click-state', 1)
+      $(this).children('.appartments-footer').css({'background-color':'#a7a37e'});
+      $(this).children('.appartments-footer').children('.appartments-footer__text--free').css({'display':'none'});
+      $(this).children('.appartments-footer').children('.appartments-footer__text--reserved').css({'display':'block'});
+    }
+
   });
+
+
+
 
 
 })();
